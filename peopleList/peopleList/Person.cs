e4ecc1b1a0f638ea1bee2ed1f53b977942bd9df0
Qandrew <andrejkrysenko@gmail.com>
2021-05-03 21:39:05 +0300

    Homework done, have questions
    Create console application where you can enter date of birth and person name
    
    Add class Person to the project.
     Class Person should consists of
     a) two fields: name and birthDate.
     b) two properties for access to these fields
     c) constructor for initialisation of these fields
     d) method GetAge(), which may calculate the age of person
    
    add verification if person has 18+ add it to List if not proceed with entering - start of the loop is missed  in half of positive (18+) cases. NEED HELP
    
    also you need to have opportunity stop entering any time (before you started enter data) and print all pesrond from list of 18+

---
+++ peopleList/peopleList/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peopleList
+{
+    class Person
+    {
+
+        
+        private string name;
+        private DateTime birthDate;
+
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        
+
+        public Person()
+        {
+            
+            Console.WriteLine("Enter name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter DOB");
+            string targetDateFormat = "dd/MM/yyyy";                    
+            string dateOfBirth = Console.ReadLine();
+            BirthDate = DateTime.ParseExact(dateOfBirth, targetDateFormat, CultureInfo.InvariantCulture);
+
+            Age = GetAge();
+            Console.WriteLine("{0} is {1} y.o.", Name, Age);
+        }
+
+
+        private int GetAge()
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - BirthDate.Year;
+            return age;
+        }
+
+
+    }
+}
---
+++ peopleList/peopleList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peopleList
+{
+    class Program
+    {
+        //enum PeopleList
+        //{
+        //    amdr = 1989,
+        //    lda = 1995,
+        //    oldman = 1955
+        //}
+        static void Main(string[] args)
+        {
+            List <Person> personsList = new List<Person>();
+            bool decision = true;
+            while (decision == true)
+            {
+                Console.WriteLine("Would you like to proceed? (Y/N)");
+                var yn = Console.ReadLine();
+                switch (yn)
+                {
+                    case "Y":
+                    case "y":
+
+                        Person chelovek = new Person();
+                        if (chelovek.Age > 18)
+                        {
+                            int age = chelovek.Age;
+                            string name = chelovek.Name;
+                            personsList.Add(new Person() { Name = name, Age = age });
+                            //personsList.AddRange(chelovek.Name, chelovek.Age);
+                        }
+
+                        break;
+                    case "N":
+                    case "n":
+                        decision = false;
+                        for(int i =0; i < personsList.Count; i++)
+                        {
+                            Console.WriteLine(personsList[i].Name + " is " + personsList[i].Age);
+                        }
+                        Console.ReadLine();
+                        break;
+                }
+            } 
+        }
+    }
+}
